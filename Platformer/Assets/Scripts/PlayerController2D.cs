0ec952777cdeeed92bc128341341b2082bf8be31
Lewis Heath <email@lewisheath.co.uk>
2022-03-29 18:45:56 +0100

    player movement can now move and jump higher with longer press

--- Platformer/Assets/Scripts/PlayerController2D.cs
+++ Platformer/Assets/Scripts/PlayerController2D.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class PlayerController2D : MonoBehaviour
 {
     Rigidbody2D rb2d;
+
+    bool isGrounded;
+    public float jumpForce;
+    public float jumpTime;
+    bool isJumping;
+    float jumpTimeCounter;
+
+    //move state
+    public float moveSpeed;
+    float moveInput;
+
+    
+    [SerializeField]
+    Transform groundCheck;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,25 +28,46 @@ public class PlayerController2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if ((Input.GetKeyDown("space") || Input.GetKeyDown("w")) && isGrounded)
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+        }
+
+        if (Input.GetKeyUp("space") || Input.GetKeyUp("w"))
+        {
+            isJumping = false;
+        }
+
+        moveInput = Input.GetAxisRaw("Horizontal");
         
     }
 
     private void FixedUpdate()
     {
-        if (Input.GetKey("w"))
+        if (isJumping)
         {
-            //jump
-            rb2d.velocity = new Vector2(rb2d.velocity.x, 10);
+            if (jumpTimeCounter > 0)
+            {
+                rb2d.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+                Debug.Log(jumpTimeCounter);
+            }
+            else
+            {
+                isJumping = false;
+            }
         }
 
-        if (Input.GetKey("d"))
-        {
-            rb2d.velocity = new Vector2(2, rb2d.velocity.y);
-        }
-
-        if (Input.GetKey("a"))
-        {
-            rb2d.velocity = new Vector2(-2, rb2d.velocity.y);
-        }
+        rb2d.velocity = new Vector2(moveInput * moveSpeed, rb2d.velocity.y);
     }
 }
